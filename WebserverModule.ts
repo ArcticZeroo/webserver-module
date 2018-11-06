3838c6baf28af9c22d6d8a288c2aa162d1b8578b
Spencer <spencer@frozor.io>
2018-11-06 13:16:33 -0500

    js -> ts

--- WebserverModule.ts
+++ WebserverModule.ts
@@ -1,9 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const EventEmitter = require("events");
-const Collection = require("@arcticzeroo/djs-collection");
-const Logger = require("frozor-logger");
-class WebserverModule extends EventEmitter {
+import * as EventEmitter from 'events';
+import * as Collection from '@arcticzeroo/djs-collection';
+import * as Logger from 'frozor-logger';
+
+export default abstract class WebserverModule extends EventEmitter {
+    private readonly _name: string;
+    public db;
+    public app: Express.Application;
+    public startByDefault: boolean;
+    public log: Logger;
+    public children: Collection;
+    public parent?: WebserverModule;
+
     /**
      * Creates a new instance of a Webserver Module.
      * <p>
@@ -28,18 +35,21 @@ class WebserverModule extends EventEmitter {
      * @param {boolean} startByDefault - Whether this module should start listening without additional method calls, default true
      * @param {string} name - The name of this module. Not required. The logger will use this name if you give it one.
      */
-    constructor({ db, app, startByDefault = true, name }) {
+    protected constructor({ db, app, startByDefault = true, name }) {
         super();
+
         this._name = name;
         this.db = db;
         this.app = app;
         this.startByDefault = startByDefault;
         this.log = new Logger(this.name);
         this.children = new Collection();
+
         if (startByDefault) {
             this.start();
         }
     }
+
     /**
      * Get this instance's name. If the name was set
      * on instantiation or with .name's setter, this
@@ -50,16 +60,17 @@ class WebserverModule extends EventEmitter {
      * that each module contains
      * @return {string|*}
      */
-    get name() {
+    get name(): string {
         return this._name || this.constructor.name;
     }
+
     /**
      * Load a child module into this module's children.
      * @param otherModule
      * @param {object} [data] - Data to load into this child. By default all props from 'this' are passed, excluding name.
      * @return {*} the child that was loaded
      */
-    loadChild(otherModule, data) {
+    loadChild(otherModule, data): any {
         if (!(otherModule instanceof WebserverModule)) {
             // Assume this is a class that can be
             // newly constructed if it's a function
@@ -68,20 +79,32 @@ class WebserverModule extends EventEmitter {
                 // if data is provided, and load data last so it can override anything we've provided
                 // already.
                 // @ts-ignore
-                otherModule = new otherModule(Object.assign({ loaderModule: this }, this, { name: null }, data));
-            }
-            else {
+                otherModule = new otherModule({ loaderModule: this, ...this, name: null, ...data });
+            } else {
                 throw new TypeError(`Invalid type given for module loading: ${typeof otherModule}`);
             }
+
             // Still not a webserver module instance
             if (!(otherModule instanceof WebserverModule)) {
                 throw new TypeError('Module given to load should be a WebserverModule.');
             }
         }
+
         otherModule.parent = this;
+
         this.children.set(otherModule.name, otherModule);
+
         return otherModule;
     }
+
+    /**
+     * This method is to be called when the module is
+     * supposed to start listening. By default it is
+     * called when the module is instantiated. Do whatever
+     * you want here.
+     * <p>
+     * If you plan to loadChild children into your module,
+     * do it here.
+     */
+    abstract start(): void;
 }
-exports.default = WebserverModule;
-//# sourceMappingURL=WebserverModule.js.map
